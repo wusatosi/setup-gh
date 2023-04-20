9c0988c1ea158ab8fab01d75a75b3abf8c3a865d
wusatosi <26424577+wusatosi@users.noreply.github.com>
2023-04-19 21:35:35 -0400

    move preflight check to script

--- install-linux.sh
+++ install-linux.sh
@@ -2,13 +2,20 @@
 
 # This script installs Github CLI according to the official guide:
 # See: https://github.com/cli/cli/blob/trunk/docs/install_linux.md
-echo "::group::Install Github CLI"
 
-type -p curl >/dev/null || (apt update && apt install curl -y)
+if type -p gh > /dev/null
+then
+  echo "Github Cli has already been installed"
+else
+  echo "::group::Install Github CLI"
 
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
-chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null &&
-apt update  && apt install gh -y
+  type -p curl >/dev/null || (apt-get update && apt-get install curl -y)
 
-echo "::endgroup::"
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg &&
+  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null &&
+  apt-get update  && apt-get install gh -y
+
+  echo "::endgroup::"
+fi
+
